0c73b1c0e626da9a2847b8fa02b1070642dacb1e
Morgan Harmon <morganharmon@gmail.com>
2022-07-10 18:24:48 -0700

    Add ability to delete rows/columns, and auto-size canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,11 +2,22 @@ import { useEffect, useState, useRef } from 'react';
 
 export default function Canvas() {
   const canvasRef = useRef(null);
-  const [tileColumns, setTileColumns] = useState([[1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,1],[1,0,0,0,0,0,1,1],[1,0,0,0,0,0,1,1],[1,0,0,0,0,0,1,1],[1,0,1,0,1,0,0,1],[1,1,1,1,1,1,1,1]]);
+  const [tileColumns, setTileColumns] = useState(
+    [
+      [1,1,1,1,1,1,1,1],
+      [1,0,0,0,0,0,0,1],
+      [1,0,0,0,0,0,1,1],
+      [1,0,0,0,0,0,1,1],
+      [1,0,0,0,0,0,1,1],
+      [1,0,1,0,1,0,0,1],
+      [1,0,1,0,1,0,0,1],
+      [1,1,1,1,1,1,1,1]]);
   const [padding, setPadding] = useState(2);
-  const [cellSize, setCellSize] = useState(30);
+  const [cellSize, setCellSize] = useState(40);
   const [numRows, setNumRows] = useState(8);
-  const [numColumns, setNumColumns] = useState(7); 
+  const [numColumns, setNumColumns] = useState(8); 
+  const [canvasWidth, setCanvasWidth] = useState(334);
+  const [canvasHeight, setCanvasHeight] = useState(334);
   
   useEffect(() => {
     const draw = ctx => {
@@ -27,29 +38,51 @@ export default function Canvas() {
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     draw(ctx);
-    },[tileColumns, cellSize, padding])
-
-    useEffect(() => {
-      if (tileColumns.length < numColumns) {
-        let newColumn = [];
-        for (let i = 0; i < numRows; i++) {
-          newColumn = [...newColumn, 1];
-        }
-        setTileColumns([...tileColumns, newColumn]);        
-      } else if (tileColumns[0].length < numRows) {
-        let newArray = [...tileColumns];
-        console.log(newArray);
-        for (let i = 0; i< newArray.length; i++) {
-          newArray[i] = [...newArray[i], 1];
+  },[tileColumns, cellSize, padding])
+  
+  useEffect(() => {
+    if (tileColumns.length < numColumns) {
+      setCanvasWidth(numColumns*(cellSize + padding) - padding);
+      setCanvasHeight(numRows*(cellSize + padding) - padding);
+      let newColumn = [];
+      for (let i = 0; i < numRows; i++) {
+        newColumn = [...newColumn, 1];
+      }
+      setTileColumns([...tileColumns, newColumn]);        
+    } else if (tileColumns[0].length < numRows) {
+      setCanvasWidth(numColumns*(cellSize + padding) - padding);
+      setCanvasHeight(numRows*(cellSize + padding) - padding);
+      let newArray = [...tileColumns];
+      for (let i = 0; i < newArray.length; i++) {
+        newArray[i] = [...newArray[i], 1];
         }
         setTileColumns(newArray);
+      } else if (tileColumns.length > numColumns) {
+        let newArray=[];
+        for (let i = 0; i < numColumns; i++) {
+          newArray[i] = [...tileColumns[i]];
+        }
+        setTileColumns(newArray);
+        setCanvasWidth(numColumns*(cellSize + padding) - padding);
+        setCanvasHeight(numRows*(cellSize + padding) - padding);
+      } else if (tileColumns[0].length > numRows) {
+        let newArray = [];
+        for (let i = 0; i < numColumns; i++) {
+          let newCol = [];
+          for (let j = 0; j < numRows; j++) {
+            newCol[j] = tileColumns[i][j];
+          }
+          newArray[i] = newCol;
+        }
+        setTileColumns(newArray);
+        setCanvasWidth(numColumns*(cellSize + padding) - padding);
+        setCanvasHeight(numRows*(cellSize + padding) - padding);
       }
     },[numRows, numColumns])
 
   return (
-    <div>
-      <canvas ref={canvasRef} id='canvas' width='600' height='600'>
-      </canvas>
+    <div id='main'>
+      <h1>Snail game level editor</h1>
       <form id='form'>
         <label>Cell size: </label>
         <input type={'number'} value={cellSize} onChange={e => setCellSize(parseInt(e.target.value))}></input>
@@ -58,6 +91,10 @@ export default function Canvas() {
       </form>
       <button onClick={() => setNumColumns(numColumns + 1)}>Add column</button>
       <button onClick={() => setNumRows(numRows + 1)}>Add row</button>
+      <button onClick={() => setNumColumns(numColumns - 1)}>Delete column</button>
+      <button onClick={() => setNumRows(numRows - 1)}>Delete row</button>
+      <canvas ref={canvasRef} id='canvas' width={`${canvasWidth}px`} height={`${canvasHeight}px`}>
+      </canvas>
         
     </div>
   );
