8d8020e598c479a76863d90abbf71dba5e25fe05
Morgan Harmon <morganharmon@gmail.com>
2022-07-11 20:52:35 -0700

    Experimenting with movement and mouseover logic, can discard

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from 'react';
 
 export default function Canvas() {
   const canvasRef = useRef(null);
+  const charRef = useRef(null);
   const [tileColumns, setTileColumns] = useState(
     [
       [1,1,1,1,1,1,1,1],
@@ -16,15 +17,17 @@ export default function Canvas() {
   const [cellSize, setCellSize] = useState(50);
   const [numRows, setNumRows] = useState(8);
   const [numColumns, setNumColumns] = useState(8); 
-  const [canvasWidth, setCanvasWidth] = useState(414); //i.v. manually calculated
-  const [canvasHeight, setCanvasHeight] = useState(414); 
+  const [canvasWidth, setCanvasWidth] = useState(() => {return numColumns*(cellSize + padding) - padding});
+  const [canvasHeight, setCanvasHeight] = useState(() => {return numRows*(cellSize + padding) - padding}); 
   const [wallColor, setWallColor] = useState('#506F91');
   const [spaceColor, setSpaceColor] = useState('#A0DAB6');
   const [coordinates, setCoordinates] = useState({});
+  const [playerCoordinates, setPlayerCoordinates] = useState({});
+  const [moveColor, setUseColor] = useState('#FFD700');
 
   useEffect(() => {
-    const tileXIndex = Math.floor((coordinates.x - canvasRef.current.offsetLeft - canvasRef.current.scrollLeft + padding) / (cellSize + padding));
-    const tileYIndex = Math.floor((coordinates.y - canvasRef.current.offsetTop - canvasRef.current.scrollTop + padding) / (cellSize + padding));
+    const tileXIndex = Math.floor((coordinates.x - canvasRef.current.offsetLeft - canvasRef.current.scrollLeft - 10 + padding) / (cellSize + padding));
+    const tileYIndex = Math.floor((coordinates.y - canvasRef.current.offsetTop - canvasRef.current.scrollTop - 10 + padding) / (cellSize + padding));
     let newArray = [];
     for (let i = 0; i < tileColumns.length; i++) {
       let newCol = [];
@@ -87,7 +90,7 @@ export default function Canvas() {
       setCanvasWidth(numColumns*(cellSize + padding) - padding);
       setCanvasHeight(numRows*(cellSize + padding) - padding);
     }
-    
+    ///////////////// draw logic ////////////////////////
     const draw = ctx => {
       tileColumns.forEach((column, i) => {
         column.forEach((tile, j) => {
@@ -151,6 +154,81 @@ export default function Canvas() {
     );
   }
 
+  const charHandleClick = (e) => {
+    console.log(e.clientX);
+    setPlayerCoordinates(
+      {
+        x: e.clientX,
+        y: e.clientY
+      }
+    );
+  }
+   
+  useEffect(() => {
+    let moveArray = [];
+    let moveXIndices = [];
+    let moveYIndices = [];
+    for (let i = 0; i < 4; i++) {
+      moveXIndices[i] = Math.floor((playerCoordinates.x - canvasRef.current.offsetLeft - canvasRef.current.scrollLeft - 10 + padding) / (cellSize + padding)) - i;
+      moveXIndices[i + 4] = Math.floor((playerCoordinates.x - canvasRef.current.offsetLeft - canvasRef.current.scrollLeft - 10 + padding) / (cellSize + padding)) + i;
+    }
+    for (let i = 0; i < 4; i++) {
+      moveYIndices[i] = Math.floor((playerCoordinates.y - canvasRef.current.offsetTop - canvasRef.current.scrollTop - 10 + padding) / (cellSize + padding)) - i;
+      moveYIndices[i + 4] = Math.floor((playerCoordinates.y - canvasRef.current.offsetTop - canvasRef.current.scrollTop - 10 + padding) / (cellSize + padding)) + i;
+    }
+    moveArray = [moveXIndices, moveYIndices];
+    console.log(moveArray[0]);
+    // console.log(tileColumns[moveArray[0][1]][moveArray[1][1]]);
+    // const draw = ctx => {
+    //   moveArray.forEach((column, i) => {
+    //     column.forEach((tile, j) => {
+    //       if (tileColumns[moveArray[0][j]][moveArray[1][j]] > 0) {
+    //         ctx.fillStyle = wallColor;
+    //         ctx.fillRect(moveArray[0][j] * (cellSize + padding), moveArray[1][j] * (cellSize + padding), cellSize, cellSize);
+    //       } else if (tile === 0) {
+    //         ctx.fillStyle = moveColor;
+    //         ctx.fillRect(moveArray[0][j] * (cellSize + padding), moveArray[1][j] * (cellSize + padding), cellSize, cellSize);
+    //       }
+    //     })
+    //   });
+    //   return ctx;
+    // }
+    // const canvas = canvasRef.current;
+    // const ctx = canvas.getContext('2d');
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // draw(ctx);
+  }, [playerCoordinates])
+
+
+  // const handleMouseMove = (e) => {
+  //   const tileXIndex = Math.floor((e.clientX - canvasRef.current.offsetLeft - canvasRef.current.scrollLeft + padding) / (cellSize + padding));
+  //   const tileYIndex = Math.floor((e.clientY - canvasRef.current.offsetTop - canvasRef.current.scrollTop + padding) / (cellSize + padding));
+  //   const canvas = canvasRef.current;
+  //   const ctx = canvas.getContext('2d');
+
+  //   setTimeout(() => {
+  //     for (let i = 0; i < tileColumns.length; i++) {
+  //       if (i === tileXIndex) {
+  //         for (let j = 0; j < tileColumns[i].length; j++) {
+  //           if (j === tileYIndex) {
+  //             ctx.shadowBlur = 5;
+  //             ctx.shadowOffsetX = 5;
+  //             ctx.shadowOffsetY = 5;
+  //             ctx.shadowColor = 'black';
+  //             if (tileColumns[tileXIndex][tileYIndex] > 0) {
+  //               ctx.fillStyle = wallColor;
+  //               ctx.fillRect(tileXIndex * (cellSize + padding), tileYIndex * (cellSize + padding), cellSize, cellSize);
+  //             } else if (tileColumns[tileXIndex][tileYIndex] === 0) {
+  //               ctx.fillStyle = spaceColor;
+  //               ctx.fillRect(i * (cellSize + padding), j * (cellSize + padding), cellSize, cellSize);
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   },0)
+  // }
+
   return (
     <div id='main'>
       <h1>Snail game map editor</h1>
@@ -173,6 +251,7 @@ export default function Canvas() {
       </div>
       <canvas onClick={(e) => handleCanvasClick(e)} ref={canvasRef} id='canvas' width={`${canvasWidth}`} height={`${canvasHeight}`}>
       </canvas>
+      <img onClick={(e) => charHandleClick(e)} ref={charRef} className='snail' src='/dream-gary1.jpg' alt='snail'></img>
         
     </div>
   );
